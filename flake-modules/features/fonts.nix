7dd37616bf8b53f2a14473a20c5f63c99fb515e5
James Lee <bruce.nunk@gmail.com>
2026-08-16 12:40:51 +1000

    Bump flake inputs

--- flake-modules/features/fonts.nix
+++ flake-modules/features/fonts.nix
@@ -4,6 +4,22 @@
   flake.modules.homeManager.fonts =
     { pkgs, ... }:
 
+    let
+      python313Packages = (pkgs.python313.override {
+        packageOverrides = _final: previous: {
+          # TODO(bump-nix): Check whether nixpkgs has updated nanoemoji's v0.16.0 source hash; remove this override when it has.
+          nanoemoji = previous.nanoemoji.overrideAttrs (_: {
+            src = pkgs.fetchFromGitHub {
+              owner = "googlefonts";
+              repo = "nanoemoji";
+              tag = "v0.16.0";
+              hash = "sha256-FysyKC01XBnRiur5RR9fcsTxQqE8x0JJHSoe3q6JtKc=";
+            };
+          });
+        };
+      }).pkgs;
+      jetbrains-mono = pkgs.jetbrains-mono.override { inherit python313Packages; };
+    in
     {
       fonts.fontconfig.enable = true;
 
