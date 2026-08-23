e22e9f76c74170b29bc527425aa36afe7ff9f919
James Lee <bruce.nunk@gmail.com>
2026-08-23 14:08:16 +1000

    Add Niri wallpaper

--- flake-modules/features/niri.nix
+++ flake-modules/features/niri.nix
@@ -4,11 +4,37 @@
   flake.modules.homeManager.niri =
     { pkgs, ... }:
 
+    let
+      # Downloaded from Adobe Stock's Free collection under its Standard License.
+      # Keep the standalone asset out of Git because that license does not permit
+      # redistributing the original file.
+      wallpaper = pkgs.requireFile {
+        name = "AdobeStock_404848752.jpeg";
+        sha256 = "sha256-zTKK0diw63b0E3nhnlrC6pW0FLAYu05G6cF6C1rWHgY=";
+        message = ''
+          The Niri wallpaper must be provisioned from its licensed download.
+
+          Source (Adobe Stock asset 404848752):
+          https://stock.adobe.com/uk/images/winter-landscape-of-a-snow-flocked-forest-jackson-hole-lake-fort-custer-state-park-michigan-usa/404848752
+
+          Add the downloaded file to the Nix store with:
+            nix-store --add-fixed sha256 /path/to/AdobeStock_404848752.jpeg
+        '';
+      };
+      wallpaperCommand = pkgs.writeShellApplication {
+        name = "niri-wallpaper";
+        runtimeInputs = [ pkgs.swaybg ];
+        text = ''
+          exec swaybg --mode fill --image "${wallpaper}"
+        '';
+      };
+    in
     {
       # Niri installed via nixos-config repo.
       # https://wiki.nixos.org/wiki/Niri/en
 
       home.packages = with pkgs; [
+        wallpaperCommand
         xwayland-satellite # xwayland support
       ];
 
