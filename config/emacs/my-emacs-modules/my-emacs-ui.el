4d2a68a01024a84caead85442e679b38cf44e9cb
James Lee <bruce.nunk@gmail.com>
2026-09-20 11:57:25 +1000

    Bump font-size from 140>150.

--- config/emacs/my-emacs-modules/my-emacs-ui.el
+++ config/emacs/my-emacs-modules/my-emacs-ui.el
@@ -92,7 +92,7 @@ error.  Evaluate the theme's package source once and retry to register it."
   (setq fontaine-presets
         '((t
            :default-family "Aporetic Serif Mono"
-           :default-height 140
+           :default-height 150
            :fixed-pitch-family "Aporetic Serif Mono"
            :fixed-pitch-height 1.0
            :variable-pitch-family "Bricolage Grotesque"
